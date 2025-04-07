1e705f89151321a3f293f7a24c6d12d6f29bb205
Nikhil Jindal <nikhil.jindal@gida.io>
2025-04-07 11:07:54 +0530

    changed the fitbit web auth

--- example/ios/Runner/GeneratedPluginRegistrant.m
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -6,16 +6,30 @@
 
 #import "GeneratedPluginRegistrant.h"
 
-#if __has_include(<flutter_web_auth/FlutterWebAuthPlugin.h>)
-#import <flutter_web_auth/FlutterWebAuthPlugin.h>
+#if __has_include(<flutter_web_auth_2/FlutterWebAuth2Plugin.h>)
+#import <flutter_web_auth_2/FlutterWebAuth2Plugin.h>
 #else
-@import flutter_web_auth;
+@import flutter_web_auth_2;
+#endif
+
+#if __has_include(<path_provider_foundation/PathProviderPlugin.h>)
+#import <path_provider_foundation/PathProviderPlugin.h>
+#else
+@import path_provider_foundation;
+#endif
+
+#if __has_include(<url_launcher_ios/URLLauncherPlugin.h>)
+#import <url_launcher_ios/URLLauncherPlugin.h>
+#else
+@import url_launcher_ios;
 #endif
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [FlutterWebAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterWebAuthPlugin"]];
+  [FlutterWebAuth2Plugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterWebAuth2Plugin"]];
+  [PathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"PathProviderPlugin"]];
+  [URLLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"URLLauncherPlugin"]];
 }
 
 @end
--- example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/harshitsingh/FlutterDev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/harshitsingh/Documents/fitbitter-2.0.2/fitbit-manual/example"
+export "FLUTTER_ROOT=/Users/nik/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/nik/fitbit-manual/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
